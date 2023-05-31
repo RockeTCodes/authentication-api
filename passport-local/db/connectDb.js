7f7654fca037dac3b30af0b2f21c06d402e6691b
Kaushal Bhardwaj <kaushalbhardwaj1125@gmail.com>
2023-06-01 00:00:00 +0530

    Updated connectDb.js comment

--- passport-local/db/connectDb.js
+++ passport-local/db/connectDb.js
@@ -3,7 +3,7 @@ mongoose.set("strictQuery", true);
 
 const connect = () => {
   try {
-    mongoose.connect("mongodb://127.0.0.1:27017/authDB");
+    mongoose.connect("mongodb://127.0.0.1:27017/authDB"); //replace it with your own url
     console.log("mongo connected");
   } catch (err) {
     console.log(err);
